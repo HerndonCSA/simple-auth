920c4a64e6c2c9521ba2e9f962342822861f5575
soos <62181284+sooswastaken@users.noreply.github.com>
2022-11-18 19:43:37 -0500

    Added tokens/announcements

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sanic import Sanic
 from sanic.response import text
 import aiosqlite
+import jwt
 
 app = Sanic("HCSA-BACKEND")
 
@@ -10,6 +11,7 @@ async def setup(app_, loop):
     app_.ctx.db = await aiosqlite.connect("database.db")
     await try_create_db(app_.ctx.db)
     app_.ctx.sessions = {}
+    app_.ctx.announcement = ""
 
 
 async def try_create_db(db):
@@ -28,21 +30,74 @@ async def cors(request, resp):
     resp.headers.update({"Access-Control-Allow-Origin": "*"})
 
 
-@app.get("/")
-async def hello_world(request):
+@app.get("/add_account")
+async def add_account(request):
+    return text("Account creation is disabled")
+    # username = request.args.get("username", None)
+    # password = request.args.get("password", None)
+    # if username and password:
+    #     # add it into the sqlite database
+    #     cursor = await request.app.ctx.db.cursor()
+    #     await cursor.execute(
+    #         "INSERT INTO  accounts VALUES (?, ?, ?)", (username, password, "test")
+    #     )
+    #     await request.app.ctx.db.commit()
+    #     await cursor.close()
+    #     return text("Added user to database")
+
+
+@app.get("/login")
+async def login(request):
     username = request.args.get("username", None)
     password = request.args.get("password", None)
     if username and password:
-        # add it into the sqlite database
+        # check if the user exists in the database
         cursor = await request.app.ctx.db.cursor()
         await cursor.execute(
-            "INSERT INTO  accounts VALUES (?, ?, ?)", (username, password, "test")
+            "SELECT * FROM accounts WHERE username=? AND password=?", (username, password)
         )
-        await request.app.ctx.db.commit()
+        user = await cursor.fetchone()
         await cursor.close()
-        return text("Added user to database")
-        return text(f"Hello {username} with password {password}")
+        if user:
+            # check if the user has an active session
+            if username in request.app.ctx.sessions:
+                # delete the old session
+                del request.app.ctx.sessions[username]
 
+            token = jwt.encode({"username": username}, "secret", algorithm="HS256")
+            request.app.ctx.sessions[username] = token
+            return text(token)
+        return text("Invalid username or password")
+    return text("Invalid username or password")
+
+
+@app.get("/logout")
+async def logout(request):
+    token = request.args.get("token", None)
+    if token:
+        for username, session_token in request.app.ctx.sessions.items():
+            if session_token == token:
+                del request.app.ctx.sessions[username]
+                return text("Logged out")
+        return text("Invalid token")
+    return text("Invalid token")
+
+
+@app.get("/announcement")
+async def announcement(request):
+    return text(request.app.ctx.announcement)
+
+
+@app.get("/set_announcement")
+async def set_announcement(request):
+    token = request.args.get("token", None)
+    if token:
+        for username, session_token in request.app.ctx.sessions.items():
+            if session_token == token:
+                request.app.ctx.announcement = request.args.get("announcement", "")
+                return text("Announcement set")
+        return text("Invalid token")
+    return text("Invalid token")
 
 
 app.register_listener(setup, "before_server_start")
